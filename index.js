d3426606fbbc10a06aec87f87d0a8e2bccbc5c6e
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-02-14 15:05:24 -0600

    working

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var inquirer = require("inquirer");
-
 var Word = require("./Word");
 
 var games=['frog', 'dog', 'cat']
 
-var game = games[0]
+var guessCount = 5;
 
-var word = new Word(game)
+var index = 0;
 
+
+var gameSetup = function(index){
+  if (index < games.length){
+    console.log("New puzzle started!")
+    var game = games[index]
+    var word = new Word(game)
+    playGame(word, guessCount)
+  }else{
+    console.log("out of puzzles")
+  }
+}
 
 var playGame = function(word, guessesLeft) {
 
@@ -18,25 +28,37 @@ var playGame = function(word, guessesLeft) {
         {
           name: "guess",
           message: "guess a letter"
+          // validate: function (name) {
+          //   return name.length === 1 && name.match(/[a-z]/);
+          // }
         }
       ]).then(function(guess) {
 
+        prevState = word.wordState()
         word.guessLetter(guess.guess)
-        console.log(word.word)
+
         console.log(word.wordState())
 
-        guessesLeft --;
+        if (word.wordState()===prevState){
+          guessesLeft --;
+        }
+        console.log(guessesLeft + " bad guesses remaining")
         playGame(word, guessesLeft)
 
       });
     }
     else {
       console.log("you win!");
+      console.log("-------------");
+      index ++;
+      gameSetup(index)
     }
   }else{
     console.log("GAME OVER: out of guesses :(")
+    console.log("-------------");
+    index ++;
+    gameSetup(index)
   }
-
 };
 
-playGame(word, 5)
+gameSetup(0)
